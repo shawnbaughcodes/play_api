ef5f81db9c4169d66d4805f911eafed212d725a6
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-12-16 18:03:02 -0600

    Made routes change

--- server/config/routes.js
+++ server/config/routes.js
@@ -15,7 +15,7 @@ module.exports = function(app){
 
 	app.get('/', Users.home);
 	app.get('/users/all/:id', Users.index);
-	app.post('/users/', Users.create);
+	app.post('/users', Users.create);
 	app.post('/sessions', Users.login);
 	app.get('/users/:id', Users.show);
 	app.post('/users/add/:id', Users.addfriend);
